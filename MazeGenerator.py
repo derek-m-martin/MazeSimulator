648e1f31debece6c581ac0aab0df3fca91b2993b
derek-m-martin <derekmartin1005@gmail.com>
2025-04-16 18:25:32 -0700

    Added a randomizeWalls function which is essentially just randomly picking one side of each cell to open up. No algorithm just straight randomness but a step in the right direction

--- MazeGenerator.py
+++ MazeGenerator.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 import sys
 from Cell import Cell
+import random
 
 pygame.init()
 
@@ -19,6 +20,15 @@ gridRows = 60
 gridCols = 60
 cellSize = 10
 mazeGrid = []
+sides = ['left', 'right', 'top', 'bottom']
+excludeTop = [0, 1, 3]
+excludeBottom = [0, 1, 2]
+excludeRight = [0, 2, 3]
+excludeLeft = [1, 2, 3]
+excludeTopAndLeft = [1, 3]
+excludeTopAndRight = [0, 3]
+excludeBottomAndLeft = [1, 2]
+excludeBottomAndRight = [0, 2]
 
 # maze dimensions/size
 maze_pixel_width = gridCols * cellSize
@@ -38,6 +48,34 @@ print(f"Generated a {len(mazeGrid)}x{len(mazeGrid[0])} grid.")
 
 # main loop
 running = True
+
+# currently randomly picks a wall to open up (1 side of each cell) NO ALGORITHM SO NOT A MAZE YET
+def randomizeWalls():
+    for i in range(gridRows):
+        for j in range(gridCols):
+            cell = mazeGrid[i][j]
+            if cell.i == 0 and cell.j == 0:
+                num = random.choice(excludeTopAndLeft)
+            elif cell.i == 0 and cell.j == 59:
+                num = random.choice(excludeTopAndRight)
+            elif cell.i == 59 and cell.j == 0:
+                num = random.choice(excludeBottomAndLeft)
+            elif cell.i == 59 and cell.j == 59:
+                num = random.choice(excludeBottomAndRight)
+            elif cell.i == 0:
+                num = random.choice(excludeTop)
+            elif cell.i == 59:
+                num = random.choice(excludeBottom)
+            elif cell.j == 0:
+                num = random.choice(excludeLeft)
+            elif cell.j == 59:
+                num = random.choice(excludeRight)
+            else:
+                num = random.randint(0, 3)
+
+            cell.changeWall(sides[num])
+
+randomizeWalls()
 
 while running:
     for event in pygame.event.get():
